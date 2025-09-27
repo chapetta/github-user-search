b52cf4d8f356bd81736a07c712b5329aa0d28d9e
chapetta <cha.petta@hotmail.com>
2025-09-27 14:30:43 -0300

    tudo funcionando bem ate aqui

--- src/contexts/theme.ts
+++ src/contexts/theme.ts
@@ -7,13 +7,19 @@ export const themeConfig = {
     layout: {
       titleColor: 'text-black',
       backgroundColor: 'bg-neutral-100',
-      bioColor: 'text-neutral-300',
+      bioColor: 'text-neutral-500',
       nameColor: 'text-black',
-      inputColor: 'text-neutral-300',
+      inputColor: 'text-neutral-700',
       twitterName: 'text-neutral-300',
       location: 'text-neutral-700',
       inputBackgroundColor: 'bg-neutral-0',
-      profileBackgroundColor: 'bg-neutral-0'
+      profileBackgroundColor: 'bg-neutral-0',
+      loginColor: 'text-blue-500',
+      dateColor: 'text-neutral-500',
+      statsBackgroundColor: 'bg-neutral-100',
+      statTitleColor: 'text-neutral-500',
+      statValueColor: 'text-black',
+      iconFilter: 'saturate(1) brightness(1) invert(0)'
     }
   },
    dark: {
@@ -23,11 +29,17 @@ export const themeConfig = {
       backgroundColor: 'bg-neutral-900',
       bioColor: 'text-neutral-100',
       nameColor: 'text-neutral-0',
-      inputColor: 'text-neutral-300',
+      inputColor: 'text-neutral-0',
       twitterName: 'text-neutral-700',
       location: 'text-neutral-100',
       inputBackgroundColor: 'bg-neutral-800',
-      profileBackgroundColor: 'bg-neutral-800'
+      profileBackgroundColor: 'bg-neutral-800',
+      loginColor: 'text-blue-300',
+      dateColor: 'text-neutral-100',
+      statsBackgroundColor: 'bg-neutral-900',
+      statTitleColor: 'text-neutral-200',
+      statValueColor: 'text-neutral-0',
+      iconFilter: 'saturate(0) brightness(0) invert(1)'
     }
   }
 }
